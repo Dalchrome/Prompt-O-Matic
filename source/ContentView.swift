31ff60cf305987c4e19812280ce4e769c71f09f5
dale <happyhaydn@gmail.com>
2024-01-20 01:34:26 +0000

    not realtime yet, added ServeOmatic options

--- source/ContentView.swift
+++ source/ContentView.swift
@@ -16,12 +16,108 @@ struct PictureThisApp: App {
 
 struct ContentView: View {
   @State var prompt:String=""
-  @ObservedObject var requester = simpleImageRequester.shared
+    @State var seed:Int = -1
+    @State var save:Bool=false
+    @State var upscale:Bool=false
+    @State var thumbnail:Bool=false
+    @State var jpeg:Bool=false
+    @State var jpglab:String = ".jpg"
+    @State var cfg :Float = 8.5
+    @State var steps :Float = 9
+    @State var filename:String = ""
+    @State var subfolder:String = ""
+    @State private var isEditing = false
+    @ObservedObject var requester = simpleImageRequester.shared
 
     var body: some View {
         VStack {
+            HStack{
+                Toggle(isOn: $upscale) {
+                    Label(" ", systemImage: "square.resize.up")
+                        .padding([.leading], -4)
+                        .padding([.trailing], -16)
+                }
+                .toggleStyle(.button)
+                Toggle(isOn: $thumbnail) {
+                    Label(" ", systemImage: "square.resize.down")
+                        .padding([.leading], -4)
+                        .padding([.trailing], -16)
+                }
+                .toggleStyle(.button)
+                Toggle(isOn: $jpeg) {
+                    Label(jpglab, systemImage: "")
+                        .padding([.leading], -12)
+                        .padding([.trailing], -2)
+                }
+                .toggleStyle(.button)
+                Toggle(isOn: $save) {
+                    Label(" ", systemImage: "square.and.arrow.down")
+                        .padding([.leading], -4)
+                        .padding([.trailing], -16)
+                }
+                .toggleStyle(.button)
+                Form {
+                    HStack {
+                        TextField(text: $filename, prompt: Text("filename")) {
+//                            Label("  ", systemImage: "doc.questionmark")
+//                                .padding([.trailing], -16)
+                        }
+                        TextField(text: $subfolder, prompt: Text("sub-folder")) {
+//                            Label("  ", systemImage: "plus.rectangle.on.folder.fill")
+//                                .padding([.trailing], -16)
+                        }
+                    }
+                }
+            }
+            HStack{
+                VStack {
+                    Slider(
+                           value: $steps,
+                           in: 1...20,
+                           step: 1
+                       ) {
+                           Text("Steps")
+                       } minimumValueLabel: {
+                           Text("0")
+                       } maximumValueLabel: {
+                           Text("20")
+                       } onEditingChanged: { editing in
+                           isEditing = editing
+                       }
+                       Text("\(steps)")
+                           .foregroundColor(isEditing ? .red : .blue)
+                }
+                VStack {
+                    Slider(
+                           value: $cfg,
+                           in: 0.1...15,
+                           step: 0.1
+                       ) {
+                           Text("CFG")
+                       } minimumValueLabel: {
+                           Text("0")
+                       } maximumValueLabel: {
+                           Text("15")
+                       } onEditingChanged: { editing in
+                           isEditing = editing
+                       }
+                       Text("\(cfg)")
+                           .foregroundColor(isEditing ? .red : .blue)
+                }
+                
+                //save square.and.arrow.down
+                //seed dice.fill
+                
+                
+                //detect som/dt
+                //models
+                //schedulers
+                //
+                Spacer()
+            }
           TextField("Prompt:", text: $prompt).onSubmit {
-            requester.runPrompt(prompt)
+                
+              requester.runPrompt(prompt, cfg: cfg, steps: steps, save: save, upscale: upscale, thumbnail: thumbnail, jpeg: jpeg, filename: filename, subfolder: subfolder)
           }
 
           if requester.image != nil {
@@ -29,6 +125,7 @@ struct ContentView: View {
           }
         }
         .padding()
+        Spacer()
     }
   }
 
@@ -37,8 +134,9 @@ class simpleImageRequester : ObservableObject {
  static var shared = simpleImageRequester()  //  make it a global, so async routines can find me
   @Published var image:NSImage?
 
-  func runPrompt(_ prompt:String){
-    SimpleDTClient.shared.runPrompt(prompt)
+    func runPrompt(_ prompt:String, cfg:Float, steps:Float, save:Bool, upscale:Bool, thumbnail:Bool, jpeg:Bool, filename :String, subfolder:String){
+      
+        SimpleDTClient.shared.runPrompt(prompt,cfg: cfg,steps: Int(steps), save: save, upscale: upscale, thumbnail: thumbnail, jpeg: jpeg, filename: filename, subfolder: subfolder)
   }
 
   func returnImage(_ imageData:Data){
--- source/TinyDTClient.swift
+++ source/TinyDTClient.swift
@@ -48,10 +48,11 @@ class SimpleDTClient : ObservableObject {
 
   // send a prompt, and optional negative prompt, async. Doesn't return anything.
   // it inserts a -1 seed
-  func runPrompt(_ posPrompt:String, negPrompt:String = ""){
-
-    // make prompt into json
-    let dictionary:[String:Any?] = ["prompt":posPrompt,"negative_prompt":negPrompt, "seed":-1]
+    func runPrompt(_ posPrompt:String, cfg:Float, steps:Int, save:Bool, upscale:Bool, thumbnail:Bool, jpeg:Bool, filename :String, subfolder:String, negPrompt:String = ""){
+      
+        
+      // make prompt into json
+        let dictionary:[String:Any?] = ["prompt":posPrompt,"negative_prompt":negPrompt, "seed":-1, "steps":Int(steps) ,"guidance_scale":Float(cfg) ,"save_images":save ,"isUpscale":upscale ,"isThumbnail":thumbnail ,"isJpeg":jpeg ,"filename":filename ,"folder":subfolder ]
 
     do {
       let myjson =  try HTTPBody.json(dictionary)
